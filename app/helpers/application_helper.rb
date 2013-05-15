062c4a7c7ed1eda6586db9d0db3565b76792e5f0
Mike Butsko <mike@buckeyeinteractive.com>
2013-05-15 15:42:02 -0400

    Add some pretty fields to the sites index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def status_helper(status)
+    case status
+    when 200..399 then content_tag :span, 'OK', :class => 'badge badge-success'
+    when 200..399 then content_tag :span, status, :class => 'badge badge-important'
+    end
+  end
+
+  def response_time_helper(time)
+    case time
+    when 0..1 then content_tag :span, (time * 1000).to_i, :class => 'badge badge-success'
+    when 1..3 then content_tag :span, (time* 1000).to_i, :class => 'badge badge-warning'
+    when 3.01..5 then content_tag :span, (time* 1000).to_i, :class => 'badge badge-important'
+    end
+  end
+
 end
